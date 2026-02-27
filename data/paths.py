d949638032d1977989eb92f8bf440aa85a89a519
hayriyeanill <anill.hayriye@gmail.com>
2026-02-27 13:18:15 +0300

    Add new folder paths.

--- data/paths.py
+++ data/paths.py
@@ -6,3 +6,6 @@ main_dir = Path(__file__).resolve().parent
 RAW_DATA_DIR = Path().resolve().parent / "data" / "raw_data"
 PROCESSED_DATA_DIR = Path().resolve().parent / "data" / "processed_data"
 SRC_DIR = Path().resolve().parent / "src"
+BASELINE_MODEL_DIR = Path().resolve().parent / "data" / "baseline_model_results"
+STATISTICAL_MODEL_DIR = Path().resolve().parent / "data" / "statistical_model_results"
+PERFORMANCE_DIR = Path().resolve().parent / "data" / "performance_results"
